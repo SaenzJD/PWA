1cf0974cf297e306a315a52575db723ea67f3c3e
SaenzJD <105192981+SaenzJD@users.noreply.github.com>
2024-03-03 23:27:22 -0700

    Add files via upload

---
+++ PWA/manifest.js
@@ -0,0 +1,34 @@
+{
+    "name": "Hell and Heaven",
+    "short_name": "Hell and Heaven",
+    "description": "Pagina de prueba, PWA",
+    "background_color": "#2B2B2B",
+    "theme_color": "#F0DB4F",
+    "orientation": "portrait",
+    "display": "standalone",
+    "start_url": "./?utm_source=web_app_manifest",
+    "scope": "./",
+    "lang": "es-MX",
+    "icons": [
+      {
+        "src": "images/android-chrome-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "images/android-chrome-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      },
+      {
+        "src": "images/favicon-32x32.png",
+        "sizes": "32x32",
+        "type": "image/png"
+      },
+      {
+        "src": "images/favicon-16x16.png",
+        "sizes": "16x16",
+        "type": "image/png"
+      }
+    ]
+  }
